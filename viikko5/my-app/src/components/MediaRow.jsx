24c9ba5d47ec74cc282ef3e2993bf80f87b41688
annagaom <122282732+annagaom@users.noreply.github.com>
2024-04-17 12:51:52 +0300

    valmis

--- viikko5/my-app/src/components/MediaRow.jsx
+++ viikko5/my-app/src/components/MediaRow.jsx
@@ -1,40 +1,33 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import Button from './UI/Button';
 
-const SingleView = props => {
-    const { selectedItem, setSelectedItem } = props;
-    const handleClick = () => {
-        setSelectedItem(null);
-    }
-    return (
-        <>
-            <dialog open={selectedItem ? true : false}>
-                <p>
-                    <button onClick={handleClick}>Close</button>
-                </p>
-                {selectedItem && (
-                    <>
-                        {selectedItem.media_type.includes('video') ?
-                            <video controls>
-                                <source src={selectedItem.filename}
-                                    type={selectedItem.media_type} />
-                            </video> : (
-                                <img src={selectedItem.filename} alt={selectedItem.title} />
-                            )}
-                        <h2>{selectedItem.title}</h2>
-                        <p>{selectedItem.description}</p>
-                        <p>Created: {new Date(selectedItem.created_at).toLocaleString('fi-FI')}</p>
-                        <p>Size: {selectedItem.filesize}</p>
-                    </>
-                )}
-            </dialog>
-        </>
-    );
+const MediaRow = (props) => {
+  const {item, setSelectedItem} = props;
+
+  const handleClick = () => {
+    setSelectedItem(item);
+  };
+
+  return (
+    <tr>
+      <td>
+        <img src={item.thumbnail} alt={item.title} />
+      </td>
+      <td>{item.title}</td>
+      <td>{item.description}</td>
+      <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
+      <td>{item.filesize}</td>
+      <td>{item.media_type}</td>
+      <td>
+        <Button text="View" handleClick={handleClick} />
+      </td>
+    </tr>
+  );
 };
 
-SingleView.propTypes = {
-    selectedItem: PropTypes.object.isRequired,
-    setSelectedItem: PropTypes.func.isRequired,
-}
+MediaRow.propTypes = {
+  item: PropTypes.object.isRequired,
+  setSelectedItem: PropTypes.func.isRequired,
+};
 
-export default SingleView;
+export default MediaRow;
